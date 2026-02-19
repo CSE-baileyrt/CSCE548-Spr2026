27b71b8df45b80142e3c69d111b51ca22d1b930c
CSE-baileyrt <143022321+CSE-baileyrt@users.noreply.github.com>
2026-02-18 19:24:28 -0500

    Add files via upload
    adding web client folder, this time

---
+++ CSharp/PbjProject/PbjWebClient/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using PbjWebClient.Models;
+using System.Diagnostics;
+
+namespace PbjWebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
